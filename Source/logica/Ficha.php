33831afc81ce64309fbb90ec128412ffdb11ceaf
ed.fernandez.gio@gmail.com <ed.fernandez.gio@gmail.com@4da6401e-218e-11de-ac9f-1d9250355bca>
2009-04-24 23:40:34 +0000

    Actualización ficha de inventario

--- Source/logica/Ficha.php
+++ Source/logica/Ficha.php
@@ -35,90 +35,140 @@ and open the template in the editor.
 
         class Inventario {
 
-            private $consultaCompras;
-            private $consultaVentas;
+            private $consultaFechasCompras;
+            private $consultaFechasVentas;
+            private $fechasCompras;
+            private $fechasVentas;
 
             function __construct(){
-                $this->consultaCompras = "select fecha, costo_unitario, cantidad from compra order by fecha";
-                $this->consultaVentas = "select fecha, costo_unitario, cantidad from venta order by fecha";
+                $this->consultaFechasCompras = "select fecha from compra order by fecha";
+                $this->consultaFechasVentas = "select fecha from venta order by fecha";
+            }
+
+            function compras(){
+                $transaccion = new TransaccionBDclass();
+
+                $resultadoFechasCompras = $transaccion->realizarTransaccion($this->consultaFechasCompras);
+
+                while ($row = mysql_fetch_array($resultadoFechasCompras)){
+                    $this->fechasCompras[] = $row["fecha"];
+                }
+            }
+
+            function ventas(){
+                $transaccion = new TransaccionBDclass();
+
+                $resultadoFechasVentas = $transaccion->realizarTransaccion($this->consultaFechasVentas);
+
+                while ($row = mysql_fetch_array($resultadoFechasVentas)){
+                    $this->fechasVentas[] = $row["fecha"];
+                }
             }
 
             function mostrarFicha(){
+                $this->compras();
+                $this->ventas();
+
                 $transaccion = new TransaccionBDclass();
+                
+                $cantidad = 1;        //Aquí van
+                $costoUnitario= 1;   //los valores del
+                $total = 1;           //inventario inicial
 
-                $resultadoCompras = $transaccion->realizarTransaccion($this->consultaCompras);
-                $resultadoVentas = $transaccion->realizarTransaccion($this->consultaVentas);
+                $i = 0;
+                $j = 0;
+                $flagCompras = true;
+                $flagVentas = true;
 
-                $cantidadUltima = 1;
-                $costoUnitarioUltimo = 1;
-                $totalUltimo = 1;
+                while (($i < count($this->fechasCompras)) || ($j < count($this->fechasVentas))) {
 
-                while (($compras = mysql_fetch_array($resultadoCompras)) ||
-                            ($ventas = mysql_fetch_array($resultadoVentas))) {
+                    
+                    if ((!$flagVentas) || ($this->fechasCompras[$i] <= $this->fechasVentas[$j])) {
+                        
+                        $fecha = $this->fechasCompras[$i];
 
-                    if (strtotime($compras["fecha"]) <= strtotime($ventas["fecha"])){
+                        $consultaCompras = "select fecha, costo_unitario, cantidad from compra where fecha = $fecha";
+
+                        $resultadoCompras = $transaccion->realizarTransaccion($consultaCompras);
                         
                         /*
                          * Colocando valores en ENTRADAS y EXISTENCIAS
                          */
 
-                        printf("<tr align = 'center'>
-                                    <td>%s</td>
-                                    <td>compra</td>
-                                    <td>%d</td>
-                                    <td>%f</td>
-                                    <td>%f</td>
-                                    <td colspan = '3'></td>
-                                    <td>%d</td>
-                                    <td>%f</td>
-                                    <td>%f</td>
-                                </tr>",
-                            $compras["fecha"],
-                            $compras["cantidad"], $compras["costo_unitario"],
-                            $compras["cantidad"] * $compras["costo_unitario"],
-                            $cantidadUltima + $compras["cantidad"],
-                            ($totalUltimo + ($compras["cantidad"] * $compras["costo_unitario"]))/($cantidadUltima + $compras["cantidad"]),
-                            $totalUltimo + ($compras["cantidad"] * $compras["costo_unitario"]));
+                        while ($compras = mysql_fetch_array($resultadoCompras)) {
 
-                        /*
-                         * Actualizando útlimos valores del inventario
-                        */
+                            echo $compras["fecha"]."<br>";
+                            echo $compras["cantidad"]."<br>";
+                            echo $compras["costo_unitario"]."<br>";
 
-                        $costoUnitarioUltimo = ($totalUltimo + ($compras["cantidad"] * $compras["costo_unitario"]))/($cantidadUltima + $compras["cantidad"]);
-                        $cantidadUltima += $compras["cantidad"];
-                        $totalUltimo += $compras["cantidad"] * $compras["costo_unitario"];
+                           /* $cantidad += $compras["cantidad"];
+                            $costoUnitario = ($total + ($compras["cantidad"] * $compras["costo_unitario"]))/($cantidad + $compras["cantidad"]);
+                            $total += ($compras["cantidad"] * $compras["costo_unitario"]);
+
+                            printf("<tr align = 'center'>
+                                        <td>%s</td>
+                                        <td>compra</td>
+                                        <td>%d</td>
+                                        <td>%f</td>
+                                        <td>%f</td>
+                                        <td colspan = '3'></td>
+                                        <td>%d</td>
+                                        <td>%f</td>
+                                        <td>%d</td>
+                                    </tr>",
+                                $compras["fecha"],
+                                $compras["cantidad"],
+                                round($compras["costo_unitario"] * 100)/100,
+                                round($compras["cantidad"] * $compras["costo_unitario"]),
+                                $cantidad, round($costoUnitario * 100)/100, $total);*/
+
+                            $i++;
+
+                            if ($i >= count($this->fechasCompras)){
+                                $flagCompras = false;
+                            }
+                        }
                     }
                     
-                    elseif (strtotime($ventas["fecha"]) < strtotime($compras["fecha"])){
+                    elseif ((!$flagCompras) || (($this->fechasCompras[$i] > $this->fechasVentas[$j]))) {
+
+                        $fecha = $this->fechasVentas[$j];
+
+                        $consultaVentas = "select * from venta where fecha = $fecha";
+
+                        $resultadoVentas = $transaccion->realizarTransaccion($consultaVentas);
 
                         /*
                          * Colocando valores en SALIDAS y EXISTENCIAS
                          */
 
-                        printf("<tr align = 'center'>
-                                    <td>%s</td>
-                                    <td>venta</td>
-                                    <td colspan = '3'></td>
-                                    <td>%d</td>
-                                    <td>%f</td>
-                                    <td>%f</td>
-                                    <td>%d</td>
-                                    <td>%f</td>
-                                    <td>%f</td>
-                                </tr>",
-                            $ventas["fecha"],
-                            $ventas["cantidad"], $costoUnitarioUltimo,
-                            $ventas["cantidad"] * $costoUnitarioUltimo,
-                            $cantidadUltima - $ventas["cantidad"],
-                            ($totalUltimo - ($ventas["cantidad"] * $costoUnitarioUltimo))/($cantidadUltima - $ventas["cantidad"]),
-                            $totalUltimo - ($ventas["cantidad"] * $costoUnitarioUltimo));
+                        /*while ($ventas = mysql_fetch_array($resultadoVentas)) {
 
-                        /*
-                         * Actualizando útlimos valores del inventario
-                        */
+                            $cantidad -= $ventas["cantidad"];
+                            $total -= ($ventas["cantidad"] * $costoUnitario);
 
-                        $cantidadUltima -= $ventas["cantidad"];
-                        $totalUltimo -= $compras["cantidad"] * $costoUnitarioUltimo;
+                            printf("<tr align = 'center'>
+                                        <td>%s</td>
+                                        <td>venta</td>
+                                        <td colspan = '3'></td>
+                                        <td>%d</td>
+                                        <td>%f</td>
+                                        <td>%f</td>
+                                        <td>%d</td>
+                                        <td>%f</td>
+                                        <td>%d</td>
+                                    </tr>",
+                                $ventas["fecha"],
+                                $ventas["cantidad"], round($costoUnitario * 100)/100,
+                                round($ventas["cantidad"] * $costoUnitario),
+                                $cantidad, round($costoUnitario * 100)/100, $total);*/
+
+                            $j++;
+
+                            if ($j >= count($this->fechasVentas)){
+                                $flagVentas = false;
+                            }
+                       // }
                     }
                 }
             }
