869db97f0448b7d8bed4863c77d1b9d373822c85
gerardobarciap <gerardobarciap@4da6401e-218e-11de-ac9f-1d9250355bca>
2009-04-26 20:44:19 +0000

    comiemzo de interfaces y objetos de dominio

--- Source/eventos/asientosDiario.php
+++ Source/eventos/asientosDiario.php
@@ -3,10 +3,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/Source/serviciotecnico/persistencia/M
 
 
 function generarFormularioNuevaCompra () {
-    $controlAsiento = new ManejadorAsiento();
-    $recursoProductos = $controlAsiento->obtenerTodosLosProductos();
     $formulario = "";
-    $formulario = '<table width="34%" border="0" cellspacing="0" cellpadding="0">
+    $formulario = '<table style="formTable" width="34%" border="0" cellspacing="0" cellpadding="0">
   <tr>
     <td colspan="2" bgcolor="#000000"><span class="style1">Nueva Compra</span></td>
   </tr>
@@ -17,7 +15,7 @@ function generarFormularioNuevaCompra () {
   </tr>
   <tr>
     <td>Proveedor:</td>
-    <td><select name="select" id="select">
+    <td><select name="proveedor" id="select">
         <option value="J-1248931-5">Epson </option>
         <option value="J-3466793-1">HP </option>
     </select>
@@ -25,19 +23,32 @@ function generarFormularioNuevaCompra () {
   </tr>
   <tr>
     <td>Producto:</td>
-    <td><select name="select2" id="select2">
+    <td><select name="producto" id="select2">
+        <option value="1">Mouse Optico </option>
+        <option value="2">Mouse Laser </option>
     </select>
     </td>
   </tr>
   <tr>
     <td>Cantidad:</td>
-    <td><input name="textfield2" type="text" id="textfield2" size="20" /></td>
+    <td><input name="cantidad" type="text" id="textfield2" size="20" /></td>
   </tr>
   <tr>
     <td>Costo Unitario:</td>
-    <td><input name="textfield3" type="text" id="textfield3" size="20" /></td>
+    <td><input name="costo" type="text" id="textfield3" size="20" /></td>
   </tr>
 </table>';
+    $objResponse = new xajaxResponse();
+    $objResponse->addAssign("control", "innerHTML", $formulario);
+    $objResponse->addScript('
+    Calendar.setup({
+        inputField     :    "f_date_c",     // id of the input field
+        ifFormat       :    "%Y-%m-%d",      // format of the input field
+        button         :    "f_trigger_c",  // trigger for the calendar (button ID)
+        align          :    "Tl",           // alignment (defaults to "Bl")
+        singleClick    :    true
+    });');
+    return $objResponse;
 }
 
 ?>
